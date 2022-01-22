8a609109e1ecaed6f94b2bd28055e493df87d782
SlejmUr <52343819+SlejmUr@users.noreply.github.com>
2022-01-22 18:35:47 +0100

    void edge update

--- LiberatorY5/VoidEdge.cs
+++ LiberatorY5/VoidEdge.cs
@@ -11,11 +11,49 @@
         public static string oldHereford_Offset = "05396158,68,28,110,D8,60,48,8";
         public static string easyDifficulty_Offset = "05390320,88,110,70,110,60,120,8,160";
         public static string house_Offset = "05393A20,90,180,F8,120,20,120,180,0";
-        public static string sitElim_Offset = "05393A20,108,180,120,68";
+        public static string elim_Offset = "05393A20,108,180,120,68";
         public static string hostage_Offset = "05393A20,120,140,40,E8,40,78";
-        public static string r6_daynight = playlistManager + "," + firstOffset + "38,0";
-        public static string r6_map = playlistManager + "," + firstOffset + "10";
-        public static string r6_gamemode = playlistManager + "," + firstOffset + "8";
-        public static string r6_difficulty = playlistManager + "," + firstOffset + "30";
+        private static string playlist = playlistManager + "," + firstOffset;
+        public static string r6_daynight = playlist + "38,0";
+        public static string r6_map = playlist + "10";
+        public static string r6_gamemode = playlist + "8";
+        public static string r6_difficulty = playlist + "30";
+
+        // All things is LONG
+
+        //day = day_Offset  |   night  = day + 32L
+
+        //dont think it is, crashed for me
+        //easy = easyDifficulty_Offset    |     normal = easy +32L   |      hard = normal + 32L 
+
+        // +32L = "=>" | -32L = "=<" | -1504L = "=)"
+        //house = house_Offset
+        //house => hereford => plane => university => favela => tower
+        //house =) club =< oregon
+        //house =) club => yacht => consulate => bank => canal => chalet => cafe => border => skyscraper => coastline => theme => villa => kasbah => outback
+
+        //gamemode
+        // +32L = "=>" | -32L = "=<" | +64L = "=)"
+        //hostage = house_Offset
+        //hostage =< bomb 
+        //hostage => secureRoom =) extractHostage => protectHostage => defuseBomb
+        //hostage => secureRoom =) extractHostage =) elimination
+
+        //elim = elim_Offset
+        //extraction = elim +32L
+        //protect = elim +64L
+        //disarm = elim +96L
+
+        //tdm = elim +128L
+        //canister = elim +160L
+
+        //camTele = elim +192L
+        //bombPrepless = elim +288L
+        //flyBy = elim +192L
+        //explosion = elim +224L
+
+        //stolenShit = elim +352L
+        //goldenGun = elim +320L
+
     }
 }

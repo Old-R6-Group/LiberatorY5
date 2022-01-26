8a7f040ed348c7a901065ea17f215b5fe2e543ca
SlejmUr <52343819+SlejmUr@users.noreply.github.com>
2022-01-26 09:51:13 +0100

    seasons

--- LiberatorY5/Sesasons.cs
+++ LiberatorY5/Sesasons.cs
@@ -241,4 +241,42 @@ namespace LiberatorY5
             return map;
         }
     }
+    internal class ShadowLegacy
+    {
+        public static string FuillBuildID = "Y5S3";
+        public static string versionCheck = "";
+        public static string versionName = "";
+        public static string playlistManager = "06E90BC0"; //or 06E3D4A0
+        public static string firstOffset = "130"; //or 100
+        public static string day_Offset = "";
+        public static string oldHereford_Offset = "";
+        public static string easyDifficulty_Offset = "";
+        public static string house_Offset = "";
+        public static string elim_Offset = "";
+        public static string hostage_Offset = "";
+        private static string playlist = playlistManager + "," + firstOffset;
+        public static string r6_daynight = playlist + ",38,0";
+        public static string r6_map = playlist + ",10";
+        public static string r6_gamemode = playlist + ",8";
+        public static string r6_difficulty = playlist + ",30";
+    }
+    internal class SteelWave
+    {
+        public static string FuillBuildID = "Y5S2";
+        public static string versionCheck = "";
+        public static string versionName = "";
+        public static string playlistManager = "065370E0"; //or 603DD10
+        public static string firstOffset = "60"; //or F8
+        public static string day_Offset = "";
+        public static string oldHereford_Offset = "";
+        public static string easyDifficulty_Offset = "";
+        public static string house_Offset = "065370E0,38,C68,E8,890,B00,1A0,430";
+        public static string elim_Offset = "065370E0,60,C8,28,0,28,0,5D0";
+        public static string hostage_Offset = "065370E0,60,C8,28,0,28,0,5B0";
+        private static string playlist = playlistManager + "," + firstOffset;
+        public static string r6_daynight = playlist + ",38,0";
+        public static string r6_map = playlist + ",10";
+        public static string r6_gamemode = playlist + ",8";
+        public static string r6_difficulty = playlist + ",30";
+    }
 }
